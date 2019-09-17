4acbf22ca4952dfd5e744fae6bbee328f0c9769b
Hugo Meiring <hugo.meiring@standardbank.co.za>
2019-09-17 21:14:02 +0200

    Changes made directly on server - never again

---
+++ scripts/galleryScript.js
@@ -0,0 +1,15 @@
+function createGallery() {
+	var prefix = "2015_";
+	var suffix = ".jpg";
+	var rowS = '<div class="col-xs-2">';
+	var rowE = '</div>'
+}
+
+
+
+
+
+<div class="row">
+	<div class="col-xs-2"><image class=".image" src="..."/></div>
+</div>
+
